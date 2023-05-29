d9380d58677f7124094b59977e24118b1ed99dea
Henrique de Lima Xavier <henrique.xavier@cwi.com.br>
2023-05-29 16:46:56 -0300

    first commit

---
+++ buildroot/modules/sstf/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/henrique/Documents/labsisop/t2/buildroot/modules/sstf/built-in.o :=  rm -f /home/henrique/Documents/labsisop/t2/buildroot/modules/sstf/built-in.o; ar rcSTPD /home/henrique/Documents/labsisop/t2/buildroot/modules/sstf/built-in.o
---
+++ buildroot/modules/sstf/.tmp_versions/sstf-iosched.mod
@@ -0,0 +1,3 @@
+/home/henrique/Documents/labsisop/t2/buildroot/modules/sstf/sstf-iosched.ko
+/home/henrique/Documents/labsisop/t2/buildroot/modules/sstf/sstf-iosched.o
+
